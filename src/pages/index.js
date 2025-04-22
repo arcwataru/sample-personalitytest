376f4e87e7d7b963d47052978542d8f12793f2a9
arcwataru <hashimoto@archetype.co.jp>
2025-04-22 17:54:48 +0900

    Delete src directory

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">MBTI診断アプリ（仮）</h1>
-      <p>Vercelにデプロイする準備が整っています。</p>
-    </div>
-  );
-}
